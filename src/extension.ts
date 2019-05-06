f2deae5963bef97af861f1ea23275a3995c95956
SoftwareApe <carl.muellerroemer@gmail.com>
2019-05-06 16:49:43 +0200

    get language standard according to extension, do nothing if extension isn't supported

--- src/extension.ts
+++ src/extension.ts
@@ -1,29 +1,57 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as path from 'path';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-        console.log('Congratulations, your extension "clang-tidy-adapter" is now active!');
-
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with registerCommand
-    // The commandId parameter must match the command field in package.json
-    const disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-        // The code you place here will be executed every time your command is executed
-
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
-    });
-
-    context.subscriptions.push(disposable);
+    const collection = vscode.languages.createDiagnosticCollection('clangTidy');
+    const workspaceFolders = vscode.workspace.workspaceFolders;
+    if (workspaceFolders !== undefined && workspaceFolders.length > 0) {
+        const workspace = workspaceFolders[0];
+        context.subscriptions.push(
+            vscode.commands.registerCommand('clangTidyAdapter.checkWarnings', checkWarnings),
+            vscode.window.onDidChangeActiveTextEditor(
+                editor => {
+                    if (undefined !== editor) {
+                        checkWarnings(workspace, editor.document, collection);
+                    }
+                },
+            ),
+        );
+    }
+    else {
+        console.info('clang-tidy adapter not activated because no workspace fodler was opened.');
+    }
 }
 
-// this method is called when your extension is deactivated
+// helper function to get configurations for this extension
+function getConfig<T>(name: string): T | undefined {
+    return vscode.workspace.getConfiguration('clangTidyAdapter').get<T>(name);
+}
+
+// get language standard, if this is undefined do nothing
+function getLanguageStandard(ext: string) : string | undefined {
+    switch (ext) {
+        case '.cpp':
+        case '.cxx':
+            return getConfig<string>('cppStandard');
+        case '.c':
+            return getConfig<string>('cStandard');
+        default:
+            return undefined;
+    }
+}
+
+function checkWarnings(
+    workspace: vscode.WorkspaceFolder,
+    document: vscode.TextDocument,
+    collection: vscode.DiagnosticCollection) {
+    // remove previous warnings from collection
+    collection.clear();
+    const languageStandard = getLanguageStandard(path.extname(document.uri.fsPath));
+    if (languageStandard !== undefined) {
+        // TODO
+    }
+}
+
 export function deactivate() {
     // Intentionally left empty
 }
