7a1dfcdf597f65f154f16105e332e9d59061b3ed
SoftwareApe <carl.muellerroemer@gmail.com>
2019-05-07 14:44:40 +0200

    working extension

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,8 @@ import * as vscode from 'vscode';
 import * as path from 'path';
 import * as child_process from 'child_process';
 import * as fs from 'fs';
+import { cpus } from 'os';
+import { SSL_OP_NETSCAPE_REUSE_CIPHER_CHANGE_BUG } from 'constants';
 
 export function activate(context: vscode.ExtensionContext) {
     const collection = vscode.languages.createDiagnosticCollection('clangTidy');
@@ -93,6 +95,78 @@ function constructCommand(workspace: vscode.WorkspaceFolder, document: vscode.Te
     return command;
 }
 
+// Clang has warning|error|note maybe others?
+function convertSeverity(severity : string) : vscode.DiagnosticSeverity {
+    switch (severity) {
+        case 'error':
+            return vscode.DiagnosticSeverity.Error;
+        case 'warning':
+            return vscode.DiagnosticSeverity.Warning;
+        default:
+            return vscode.DiagnosticSeverity.Hint;
+    }
+}
+
+// Clang only returns line adn position
+function convertPosition(line: number, position: number) : vscode.Range {
+    // change from number to offset
+    line -= 1;
+    position -= 1;
+    return new vscode.Range(line, position, line, position + 1);
+}
+
+function parseWarnings(rawWarnings: string) : Map<string, vscode.Diagnostic[]> {
+    const lines = rawWarnings.split('\n');
+    const regex : RegExp = /(.*):(\d+):(\d+):\s+(warning|note|error):\s+(.*)/i;
+    const matches : RegExpMatchArray[] = [];
+    lines.forEach(
+        line => {
+            const match = line.match(regex);
+            if (match !== null) {
+                matches.push(match);
+            }
+        },
+    );
+
+    const allDiagnostics : [string, vscode.Diagnostic][] =
+        matches.map(
+            m => {
+                const file = m[1];
+                const line = parseInt(m[2], 10);
+                const position = parseInt(m[3], 10);
+                const severity = m[4];
+                const message = m[5];
+
+                const diagnostic =
+                    new vscode.Diagnostic(
+                        convertPosition(line, position),
+                        message,
+                        convertSeverity(severity),
+                    );
+
+                return [file, diagnostic];
+            },
+        );
+
+    // group by uri
+    const result = new Map<string, vscode.Diagnostic[]>();
+    allDiagnostics.forEach(
+        v => {
+            const file = v[0];
+            const diag = v[1];
+            const prev = result.get(file);
+            if (prev !== undefined) {
+                prev.push(diag);
+                result.set(file, prev);
+            }
+            else {
+                result.set(file, [diag]);
+            }
+        },
+    );
+    return result;
+}
+
 function updateDiagnostics(rawWarnings: string, collection: vscode.DiagnosticCollection) {
     // TODO
 }
@@ -101,8 +175,6 @@ function checkWarnings(
     workspace: vscode.WorkspaceFolder,
     document: vscode.TextDocument,
     collection: vscode.DiagnosticCollection) {
-    // remove previous warnings from collection
-    collection.clear();
 
     const command = constructCommand(workspace, document);
     if (command === undefined) {
@@ -119,7 +191,15 @@ function checkWarnings(
                 console.error(stderr.toString());
             }
             const rawWarnings = stdout.toString();
-            updateDiagnostics(rawWarnings, collection);
+            const parsedWarnings = parseWarnings(rawWarnings);
+            parsedWarnings.forEach(
+                (value, key) => {
+                    // remove previous warnings from collection
+                    const uri = vscode.Uri.file(key);
+                    collection.delete(uri);
+                    collection.set(uri, value);
+                },
+            );
         },
     );
 }
